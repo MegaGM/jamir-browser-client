65594f3cde240bdbe72be617086e106de4065053
Mega <Aza.Noriega@gmail.com>
2019-11-16 03:58:58 +0500

    better types for AddressProgram

--- src/types/AddressProgram.d.ts
+++ src/types/AddressProgram.d.ts
@@ -1,6 +1,10 @@
+import AddressProgramRow from './AddressProgramRow'
+
 export default interface AddressProgram {
   _id: string
   title: string
   editable?: boolean
   initialTitle?: string
+  rows?: Array<AddressProgramRow>
+  rowCount: number
 }
---
+++ src/types/AddressProgramRow.d.ts
@@ -0,0 +1,7 @@
+export default interface AddressProgramRow {
+  _id: string
+  city: string
+  address: string
+  streetName: string
+  buildingBaseNumber: number
+}
